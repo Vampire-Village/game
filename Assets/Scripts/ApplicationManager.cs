069dad1affb9b21c177076def862533fbe3bc80e
Nadya Djojosantoso <hello@nadyafebi.com>
2020-10-31 00:27:03 -0700

    Add start menu layout and saveable player name

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handles getting and setting data that persists when the game is closed and opened again.
+/// </summary>
+public static class ApplicationManager
+{
+    private const string k_PlayerNameKey = "PlayerName";
+
+    public static string GetPlayerName()
+    {
+        return PlayerPrefs.GetString(k_PlayerNameKey);
+    }
+
+    public static void SetPlayerName(string name)
+    {
+        PlayerPrefs.SetString(k_PlayerNameKey, name);
+    }
+}
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button hostButton;
+    public Button joinButton;
+    public TMP_InputField nameInput;
+    public TMP_InputField roomInput;
+    
+    void Start()
+    {
+        // Handle getting and saving name.
+        nameInput.text = ApplicationManager.GetPlayerName();
+        nameInput.onEndEdit.AddListener((newName) =>
+        {
+            ApplicationManager.SetPlayerName(newName);
+        });
+    }
+}
