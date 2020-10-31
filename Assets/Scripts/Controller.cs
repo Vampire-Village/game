4b508b20083fdc197d9897255e6aa691e872c8bd
leotheshade <leotheshade@gmail.com>
2020-10-30 22:04:42 -0700

    Added movement and camera follow features
    Added a basic movement script, and set the camera to follow the character.
    Going to import Cinemachine to get better functionality for the camera next.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+    void Start()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        float moveXAxis = Input.GetAxis("Horizontal");
+        float moveZAxis = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveXAxis, 0, moveZAxis);
+        movement = Vector3.ClampMagnitude(movement, 1f);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
