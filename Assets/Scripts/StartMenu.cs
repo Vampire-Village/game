2c76bb2c0bac792ba5481e5ccbd7d4561dc1a8b2
Nadya Djojosantoso <hello@nadyafebi.com>
2020-11-02 00:13:49 -0800

    Add online & offline states, improve UI prefabs

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -4,18 +4,81 @@ using TMPro;
 
 public class StartMenu : MonoBehaviour
 {
+    [Header("Server Only")]
+    public GameObject serverPanel;
+
+    [Header("Client Only")]
+    public TMP_InputField nameInput;
+    public GameObject connectingPanel;
+
+    [Header("Online State")]
+    public GameObject onlinePanel;
     public Button hostButton;
     public Button joinButton;
-    public TMP_InputField nameInput;
     public TMP_InputField roomInput;
-    
-    void Start()
+
+    [Header("Offline State")]
+    public GameObject offlinePanel;
+    public Button reconnectButton;
+
+    private VampireVillageNetwork network;
+
+    private void Awake()
     {
-        // Handle getting and saving name.
-        nameInput.text = ApplicationManager.GetPlayerName();
-        nameInput.onEndEdit.AddListener((newName) =>
+        network = VampireVillageNetwork.instance;
+        network.OnNetworkStart += OnNetworkStart;
+    }
+
+    private void OnNetworkStart(bool isServer)
+    {
+        if (isServer)
         {
-            ApplicationManager.SetPlayerName(newName);
-        });
+            onlinePanel.SetActive(false);
+            offlinePanel.SetActive(false);
+            connectingPanel.SetActive(false);
+            nameInput.gameObject.SetActive(false);
+            serverPanel.SetActive(true);
+        }
+        else
+        {
+            // Show connecting panel.
+            offlinePanel.SetActive(false);
+            onlinePanel.SetActive(false);
+            connectingPanel.SetActive(true);
+
+            // Handle getting and saving name.
+            nameInput.text = ApplicationManager.GetPlayerName();
+            nameInput.onEndEdit.AddListener((newName) =>
+            {
+                ApplicationManager.SetPlayerName(newName);
+            });
+
+            // Handle reconnecting.
+            reconnectButton.onClick.AddListener(() => network.StartClient());
+
+            network.OnNetworkOnline += OnNetworkOnline;
+            network.OnNetworkOffline += OnNetworkOffline;
+        }
+    }
+
+    private void OnNetworkOnline()
+    {
+        connectingPanel.SetActive(false);
+        offlinePanel.SetActive(false);
+        onlinePanel.SetActive(true);
+    }
+
+    private void OnNetworkOffline()
+    {
+        connectingPanel.SetActive(false);
+        onlinePanel.SetActive(false);
+        offlinePanel.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        network.OnNetworkStart -= OnNetworkStart;
+        network.OnNetworkOnline -= OnNetworkOnline;
+        network.OnNetworkOffline -= OnNetworkOffline;
     }
 }
--- Assets/Scripts/VampireVillageNetwork.cs
+++ Assets/Scripts/VampireVillageNetwork.cs
@@ -1,12 +1,24 @@
-﻿using Mirror;
+﻿using System;
+using Mirror;
 using ParrelSync;
 
 public class VampireVillageNetwork : NetworkManager
 {
+    public static VampireVillageNetwork instance { get; private set; }
+
+    public event Action<bool> OnNetworkStart;
+    public event Action OnNetworkOnline;
+    public event Action OnNetworkOffline;
+
+    public override void Awake()
+    {
+        base.Awake();
+        instance = this;
+    }
+
     public override void Start()
     {
         base.Start();
-        showDebugMessages = false;
 
         // Setup for ParrelSync.
     #if UNITY_EDITOR
@@ -17,31 +29,40 @@ public class VampireVillageNetwork : NetworkManager
     #endif
     }
 
-#region Server Hooks
+    #region Server Hooks
 
     public override void OnStartServer()
     {
+        OnNetworkStart?.Invoke(true);
         GameLogger.LogServer("Started!");
     }
 
-#endregion
+    #endregion
 
-#region Client Hooks
+    #region Client Hooks
+
+    public override void OnStartClient()
+    {
+        OnNetworkStart?.Invoke(false);
+        GameLogger.LogClient("Connecting to the server...");
+    }
 
     public override void OnClientConnect(NetworkConnection conn)
     {
+        OnNetworkOnline?.Invoke();
         GameLogger.LogClient("Successfully connected to the server!");
     }
 
     public override void OnClientDisconnect(NetworkConnection conn)
     {
+        OnNetworkOffline?.Invoke();
         GameLogger.LogClient("Disconnected from the server.");
 
         // TODO: Retry connecting instead.
         StopClient();
     }
 
-#endregion
+    #endregion
 
     public override void OnApplicationQuit()
     {
