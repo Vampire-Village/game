ebf19f7e8cb73c951fb818671489b321bd0864b1
Nadya Djojosantoso <hello@nadyafebi.com>
2020-11-21 11:56:02 -0800

    Add Player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+/// <summary>
+/// TODO:
+/// - Change from MonoBehavior -> NetworkBehavior
+/// - Get/set the player current role here.
+/// </summary>
+public class Player : MonoBehaviour
+{
+    public static Player local;
+
+    public void Awake()
+    {
+        local = this;
+    }
+
+    // TODO: Switch to this once networking code catches up.
+    // public override void OnStartAuthority()
+    // {
+    //     local = this;
+    // }
+}
