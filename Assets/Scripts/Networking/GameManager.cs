eccc70c18210b7f76cf2734757ad98dbfb772cbe
Nadya Djojosantoso <hello@nadyafebi.com>
2020-12-03 20:14:17 -0800

    Fix spawn point going out of range

--- Assets/Scripts/Networking/GameManager.cs
+++ Assets/Scripts/Networking/GameManager.cs
@@ -99,7 +99,7 @@ namespace VampireVillage.Network
 
                 // Move player to spawn points.
                 // TODO: Make it random.
-                gamePlayer.TargetSetPosition(spawnPoints[i % room.players.Count].transform.position);
+                gamePlayer.TargetSetPosition(spawnPoints[i % spawnPoints.Count].transform.position);
             }
         }
 
