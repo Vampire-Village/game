8eb9af772055b5480e329f121d9b17b816657b93
hungbuiwork <hungbuiwork@gmail.com>
2021-01-07 17:29:15 -0800

    Started Lobby Map
    Also rearranged and organized some of art folders

--- Assets/Scripts/Lighting Scripts (Hung Just trying stuff out)/Fog Change Function.cs
+++ Assets/Scripts/Lighting Scripts (Hung Just trying stuff out)/Fog Change Function.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +7,6 @@ public class FogChangeFunction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
