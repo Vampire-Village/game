1bd5308a4ef8e4842f56024f921596385c9fa7ed
hungbuiwork <hungbuiwork@gmail.com>
2021-01-15 21:27:52 -0800

    Lights script, test with "z"
    Added lights script which includes a function that blends from day to night smoothly. Its currently testable with the input "Z" lolol.

---
+++ Assets/Lights.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    public Light light;
+    [SerializeField] [Range(0f,0.1f)] float transitionTime;
+    public Color activeColor;
+    public Color dayLight;
+    public Color nightLight;
+    public Color activeFog;
+    public Color dayFog;
+    public Color nightFog;
+    bool isDay = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            DayNight();
+        }
+        light.color = Color.Lerp(light.color, activeColor, transitionTime);
+        RenderSettings.fogColor = Color.Lerp(RenderSettings.fogColor, activeFog, transitionTime);
+
+    }
+
+    public void DayNight()
+    {
+        isDay = !isDay;
+        if (isDay)
+        {
+            activeColor = dayLight;
+            activeFog = dayFog;
+        }
+        else
+        {
+            activeFog = nightFog;
+            activeColor = nightLight;
+        }
+    }
+}
