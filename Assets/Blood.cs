a01fd2c9d96c87074cc29b12ff011b0c2d590b43
hungbuiwork <hungbuiwork@gmail.com>
2021-01-24 14:35:03 -0800

    Added Blood trails
    2 scripts, one which makes blood dissappear slowly over time(the duration can be modified), and another which spawns the blood on the ground

---
+++ Assets/Blood.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blood : MonoBehaviour
+{
+    public SpriteRenderer bloodRender;
+    public float fadeTime = 10f;
+    private float currentAlpha = 255;
+    private float alphaChange = 0;
+    private float timeSinceChange = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bloodRender = GetComponentInChildren<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alphaChange += (Time.deltaTime * 1 / fadeTime);
+        currentAlpha = 1 - alphaChange;
+        timeSinceChange += Time.deltaTime;
+        if (timeSinceChange > 1)
+        {
+            bloodRender.color = new Color(bloodRender.color.r, bloodRender.color.g, bloodRender.color.b, currentAlpha);
+            timeSinceChange = 0;
+            if(currentAlpha <= 0)
+            {
+                Destroy(gameObject);
+            }
+                  
+        }
+        
+    }
+}
---
+++ Assets/BloodSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodSpawner : MonoBehaviour
+{
+    public bool isBleeding = true;
+    public Transform player;
+    public GameObject bloodPrefab; //need to manually drag the prefab in
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Transform>();
+    }
+    //STILL NEEDS: to randomize spawntimes, as well as randomize size
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBleeding)
+        {
+            
+            time += Time.deltaTime;
+            if (time > 0.5)
+            {
+                SpawnBlood();
+                time = 0;
+            }
+        }
+        
+    }
+    RaycastHit ShootDownwardRaycast()
+    {
+        RaycastHit hit;
+        Physics.Raycast(player.position, -Vector3.up, out hit);
+        //Debug.DrawRay(player.position, -Vector3.up, new Color(1f, 0f, 0f, 1f));
+        return hit;
+    }
+    void SpawnBlood()
+    {
+        Vector3 raycast = ShootDownwardRaycast().point;
+        Instantiate(bloodPrefab, new Vector3(raycast.x, raycast.y + 0.05f, raycast.z), Quaternion.identity);
+        
+    }
+
+}
