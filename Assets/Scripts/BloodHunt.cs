dc0c3b9472478ce0fdb3f80d7827dd2a06dca10e
hungbuiwork <hungbuiwork@gmail.com>
2021-04-02 19:48:44 -0700

    Blood Hunt Updated
    Changed the Trail Renderer to be in Prefab, but currently does not distinguish between roles, thus activating on all players.

--- Assets/Scripts/BloodHunt.cs
+++ Assets/Scripts/BloodHunt.cs
@@ -8,6 +8,7 @@ using System.Linq;
 public class BloodHunt : MonoBehaviour
 {
     public int range = 10;
+    public int secondsBloodLasts = 5;
     public GameObject[] playersInScene;
     public GameObject[] playerInRange;
     public TrailRenderer trail;
@@ -15,41 +16,43 @@ public class BloodHunt : MonoBehaviour
     void Start()
     {
         playersInScene = GameObject.FindGameObjectsWithTag("Player");
-        Initialize_Trails();
+        Initialize_Blood_settings();
     }
 
     // Update is called once per frame
     void Update()
     {
+        playersInScene = GameObject.FindGameObjectsWithTag("Player");
         Update_Player_In_Range();
         RenderBlood();
+    }
+    void Initialize_Blood_settings()
+    {
+        foreach (GameObject player in playersInScene)
+        {
+            TrailRenderer trail = player.GetComponentInChildren<TrailRenderer>();
+            trail.time = secondsBloodLasts;
+        }
     }
     void Update_Player_In_Range()
     {
         //Updates the player within range, from the list of all players loaded at the beginning of scene.
         playerInRange = (from player in playersInScene where (Vector3.Distance(player.transform.position, this.transform.position) < range) select player).ToArray();
     }
-    void Initialize_Trails()
-    {
-        //UNFINISHED: Initialization of trails for all players. Considering adding trail to villager prefab instead. 
-        foreach(GameObject player in playersInScene)
-        {
-            TrailRenderer tr = player.AddComponent<TrailRenderer>();
-            //Copy the trail renderer onto all players
-
-        }
-    }
+    
     void RenderBlood()
     {   
         //Changes Player Trail Render to either True or False depending if within range.
         foreach (GameObject player in playersInScene)
         {
+            TrailRenderer trail = player.GetComponentInChildren<TrailRenderer>();
+            trail.enabled = true;
             if (playerInRange.Contains(player)){
-                player.GetComponent<TrailRenderer>().enabled = true;
+                trail.emitting = true;
             }
             else
             {
-                player.GetComponent<TrailRenderer>().enabled = false;
+                trail.emitting = false;
             }
         }
     }
