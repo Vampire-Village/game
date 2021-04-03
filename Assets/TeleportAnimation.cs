853ba9829f82e320cd7e3a7b77159ddc8d79adcc
hungbuiwork <hungbuiwork@gmail.com>
2021-04-02 20:14:31 -0700

    Teleport Animation Script Added to Prefab
    Currently triggers on "Space". Needs to be referenced in the teleport script!

---
+++ Assets/TeleportAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//REFERENCE THIS SCRIPT AND USE PLAYANIMATION TO PLAY THE TELEPORT ANIMATION. THE CONTENTS OF THIS PARTICLE SYSTEM CAN BE ACTIVATED/DEACTIVATED
+//DEPENDING ON WHICH ANIMATIONS YOUD LIKE TO SHOW/HAVE. COULD BE CUSTOMIZABLE IN THE FUTURE
+//NOTE: CURRENTLY IS ACTIVATABLE WITH INPUT KEY SPACE SO DEACTIVATE THIS ONCE ADDED TO THE GRAVE SCRIPT.
+public class TeleportAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayAnimations();
+        }
+    }
+    void PlayAnimations()
+    {
+        GetComponent<ParticleSystem>().Play(true);
+    }
+}
