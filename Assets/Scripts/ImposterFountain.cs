4c298975729ef4a3eebcb42f86ded546ba0033ba
Autisticturtle <1046264@gmail.com>
2021-04-11 21:13:27 -0700

    ImposterFountaintask that's not yet useful

---
+++ Assets/Scripts/ImposterFountain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ImposterFountainController : Task
+{
+    public GameObject Rock;
+    private int Rocknum;
+    public bool taskComplete = false;
+    public Item slowdownTimer;
+    public GameObject playerReference;
+
+
+    public void Start()
+    {
+        Rocknum = 0;
+        //playerReference = TaskSpawner.currentPlayer();
+    }
+
+    public void OnGUI()
+    {
+        //bool buttonStatus = PourButton.GetComponent<MouseHoldController>().isMouseHeld();
+        if (!taskComplete)
+        {
+            Rocknum += 1;
+            if (Rocknum > 5)
+            {
+                taskComplete = true;
+                CompleteTask(gameObject, slowdownTimer);
+            }
+            //Debug.Log(candleTop);
+        }
+    }
+
+
+
+
+}
