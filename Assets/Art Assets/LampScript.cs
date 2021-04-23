26220ad8af937cf826d3d7ea0e353ce745b8e5a8
hungbuiwork <hungbuiwork@gmail.com>
2021-04-23 14:43:51 -0700

    Ghost: Lamp switch on/off (unfinished)
    A function that can be called on the lamp that turns it on or off. Having issues with material-swapping.

---
+++ Assets/Art Assets/LampScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Unfinished, have no clue how to get this material swapping to work.
+/// </summary>
+public class LampScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Material onMaterial;
+    public Material offMaterial;
+    public MeshRenderer lamp;
+    public GameObject light;
+    bool lighton = true;
+    void Start()
+    {
+        lamp = GetComponent<MeshRenderer>();
+        lamp.materials[1] = onMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for testing
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Switch();
+        }
+    }
+    void Switch()
+    {
+        lighton = !lighton;
+        if (lighton)
+        {
+            lamp.materials[1] = offMaterial;
+            light.SetActive(false);
+        }
+        else
+        {
+            lamp.materials[1] = onMaterial;
+            light.SetActive(true);
+        }
+    }
+
+}
