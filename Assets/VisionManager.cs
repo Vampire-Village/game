da3b9244bd5752815a90f04c972c9c792be3043a
hungbuiwork <hungbuiwork@gmail.com>
2021-05-07 13:32:51 -0700

    Finished VisionManager Script BUT
    needs to be tested in actual game. I've only tested it with a psuedoPrefab

---
+++ Assets/VisionManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using VampireVillage.Network;
+public class VisionManager : MonoBehaviour
+{
+    //Attach this script to the parent prefab. Make sure to assign the parentSprite via drag and click!
+    // Start is called before the first frame update
+    public Camera camera;
+    public GamePlayer gamePlayer;
+    public Transform parentSprite;
+    int ghostViewable = 11;
+    int vampireViewable = 10;
+    void Start()
+    {
+        camera = GetComponentInChildren<Camera>();
+        gamePlayer = GamePlayer.local;
+        gamePlayer.OnRoleUpdated.AddListener(UpdateVisualsForRole);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UpdateVisualsForRole();
+        }
+    }
+
+    void SetSpriteLayer(LayerMask layer)
+    {
+        //set children of the parent sprite to specific layer
+        foreach(Transform sprite in parentSprite)
+        {
+            sprite.gameObject.layer = layer;
+        }
+    }
+    void UpdateVisualsForRole()
+    {
+        //Notes: Vampire Lords can see what villagers see + blood trails
+        //Infected can see what villagers see
+
+        switch (gamePlayer.role)
+        {
+            case Role.VampireLord:
+                camera.cullingMask = ~(1 << ghostViewable);
+                break;
+            case Role.Ghost:
+                SetSpriteLayer(ghostViewable);
+                break;
+            default:
+                camera.cullingMask = ~((1 << ghostViewable) | (1 << vampireViewable));
+                break;
+        }
+    }
+}
