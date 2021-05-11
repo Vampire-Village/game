b966a95bb0c14c077de3387178541f6de06a7f63
Kari Tu <kari.tttran@gmail.com>
2021-05-10 17:07:22 -0700

    Ping Functions

--- Assets/Scripts/Infected.cs
+++ Assets/Scripts/Infected.cs
@@ -1,25 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using VampireVillage.Network;
+using TMPro;
 
-public class Infected : MonoBehaviour
+public class Infected : NetworkBehaviour
 {
-
     public GameObject pingSprite;
-
     private float xPos;
     private float zPos;
-
     private GameObject minimap;
-
-    //private int cooldown = 5;
-    private int cdSec = 0;
 
     private PlayerUI playerUI;
 
     private float pingHeight = 20;
     private float pingZoffset = 1.1f;
-
 
     public void RegisterUI(PlayerUI playerUI)
     {
@@ -28,31 +24,19 @@ public class Infected : MonoBehaviour
 
     public void Ping()
     {
-        //if (cdSec == 0)
-        //{
-            //cdSec = cooldown;
-            //StartCoroutine(LowerCooldown());
-            Debug.Log("*ping*");
-            minimap = GameObject.Find("/UI Canvas/VampireMinimap"); // move to start when testing actual game
-            xPos = GetComponent<Transform>().position.x;
-            zPos = GetComponent<Transform>().position.z;
-            // may need to adjust y/z positions
-            GameObject ping = Instantiate(pingSprite, new Vector3(xPos, pingHeight, zPos + pingZoffset), Quaternion.Euler(90, 0, 0)) as GameObject;
-            ping.transform.parent = minimap.transform;
-        //}
+        
+        Debug.Log("*ping*");
+        //minimap = GameObject.Find("/UI Canvas/VampireMinimap"); // move to start when testing actual game
+        xPos = GetComponent<Transform>().position.x;
+        zPos = GetComponent<Transform>().position.z;
+        GameObject ping = Instantiate(pingSprite, new Vector3(xPos, pingHeight, zPos + pingZoffset), Quaternion.Euler(90, 0, 0)) as GameObject;
+        //ping.transform.parent = minimap.transform;
+        CmdPing(ping);
     }
 
-    IEnumerator LowerCooldown()
+    [Command]
+    private void CmdPing(GameObject ping)
     {
-        while (cdSec > 0)
-        {
-            // Change button text
-            playerUI.pingText.text = "Ping (" + cdSec + ")";
-
-            yield return new WaitForSeconds(1);
-            cdSec -= 1;
-        }
-        playerUI.pingText.text = "Ping";
+        GameManager.local.Ping(ping);
     }
-
 }
